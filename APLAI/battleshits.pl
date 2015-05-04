3c59cb3572ae8d7887c47fe40732a10a255bb8cf
WoutV <w.vekemans@gmail.com>
2015-05-04 17:17:31 +0200

    wenen :'(

--- APLAI/battleshits.pl
+++ APLAI/battleshits.pl
@@ -1,5 +1,6 @@
 :- lib(ic).
 :- lib(lists).
+:- lib(ic_global).
 
 battleshits(Hints, Rows, Columns,Solution):-
     initialize_boats(Hints,Solution),
@@ -11,10 +12,11 @@ battleshits(Hints, Rows, Columns,Solution):-
 	check_boats(Solution),
     check_rows(Rows,Solution),
     check_columns(Columns,Solution),
+    write('searching'),nl,
 	search(OneList,0,input_order,indomain,complete,[backtrack(B)]),
-    pretty_print(Solution, Rows, Columns).
+    pretty_print(Solution, Rows, Columns,B).
 	
-pretty_print(Solution, Rows, Columns):-
+pretty_print(Solution, Rows, Columns,B):-
 	(for(N,1,10),
     param(Solution), param(Rows)
     do
@@ -39,32 +41,182 @@ writeRow(List):-
 ).
 	
 check_water(List,Amount):-
-	count(0,List,Amount).
+	occurrences(0,List,Amount).
     
 check_boats(Solution):-
 	List is Solution[1..10,1..10],
 	createOneList(List,OneList),
-    check_submarines(OneList),
-	check_middlepieces(OneList),
-	%check_ends(OneList),
-	check_length2(Solution, 3).
-	
+	%% check_length2(Solution, 3),
+	%% check_length3(Solution,2),
+	check_length4(Solution,1),
+ %%    check_submarines(OneList),
+    no_touching(Solution).
+
+
+no_touching(Solution):-
+	for(N,1,10),
+	param(Solution)
+    do
+    	Row is Solution[N,1..10],
+    	write('roooow'),nl,
+    	check_row_touching(Row),
+    	(N>1 ->
+    		(Above is N-1,
+    		RowAbove is Solution[Above,1..10],
+    		write('above'),nl,
+    		check_above(Row,RowAbove)
+    		)
+    		;
+    		true
+    	)
+    	%% ),
+		%% (N=\=10 ->
+		%% 	(Below is N+1,
+		%% 	RowBelow is Solution[Below,1..10],
+		%% 	check_below(Row,RowBelow)
+		%% 	)
+		%% 	;
+		%% 	true
+	.
+
+check_above(Row,Above):-
+	for(N,1,10),
+	param(Row),param(Above)
+	do
+		write(N),nl,
+		get_element(Row,N,Elem),
+		get_element(Above,N,AboveEl),
+		Left is N-1,
+		Right is N+1,
+		get_element(Above,Left,LeftElem),
+		get_element(Above,Right,RightElem),
+		check_diagonal(LeftElem,Elem),
+		check_diagonal(RightElem,Elem), 
+		check_straight_above(AboveEl,Elem).
+
+
+check_straight_above(Above,Elem):-
+	write('straight'),nl,
+	write(Above),nl,write(Elem),nl,
+	#=(Elem,0,ElemZero),
+	#=(Above,0,AboveZero),
+	#\=(Above,4,AboveNotFour),
+	#\=(Elem,0,ElemNotZero),
+
+	#=(Above,4,AboveTop),
+	#=(Elem,5,ElemBottom),
+	#=(Elem,2,ElemMid),
+	#=(Above,2,AboveMid),
+	or(ElemBottom,ElemMid,ElemBotOrMid),
+	and(AboveTop,ElemBotOrMid,ElementBotOrMid),
+	and(AboveMid,ElemBotOrMid,AboveIsMid),
+	and(ElemZero,AboveNotFour,ElementIsWater),
+	and(ElemNotZero,AboveZero,AboveIsZero),
+
+
+	sumOfList([ElementIsWater,AboveIsZero,ElementBotOrMid,AboveIsMid],1).
+
+
+check_diagonal(E,Other) :-
+	write('check_diagonal'),nl,
+	#\=(E,0,ENotZero),
+	#\=(Other,0,ONotZero),
+	#=(E,0,EZero),
+	#=(Other,0,OZero),
+	and(ENotZero,OZero,B1),
+	and(EZero,ONotZero,B2),
+	and(EZero,OZero,BothZero),
+	#\=(B1,B2,OneOfBoth),
+	sumOfList([BothZero,OneOfBoth],1).
+
+check_row_touching(List):-
+	for(N,2,9),
+	param(List)
+	do
+		get_element(List,N,Elem),
+		Left is N-1,
+		Right is N+1,
+		get_element(List,Left,LeftElem),
+		get_element(List,Right,RightElem),
+		checkLeft(Elem,LeftElem),
+		checkRight(Elem,RightElem).
+
+
+%% Checks whether or not the elements are compatible. The given LeftElement is located on
+%% the left of Element
+checkLeft(Element,LeftElement):-
+	#>(LeftElement,2,B1),
+	#=(Element,0,B2),
+	and(B1,B2,ElementIsWater),
+
+	#<(LeftElement,3,B3),
+	#\=(LeftElement,0,B4),
+	and(B3,B4,ElisLorM),
+	#=(Elem,3,El3),
+	#=(Elem,2,El2),
+	or(El3,El2,ElemMorR),
+	and(ElemMorR,ElisLorM,ElementIsRightOrMid),
+
+	#=(LeftElement,0,LeftWater),
+	#\=(Elem,3,ElemNotR),
+	and(LeftWater,ElemNotR,ElementNotR),
+	sumOfList([ElementIsWater,ElementIsRightOrMid,ElementNotR],1).
+
+%% Checks whether or not the elements are compatible. The given RightElement is located on
+%% the right of Element
+checkRight(Element,RightElement):-
+	#>(RightElement,3,B1), 
+	#=(RightElement,1,B2), 
+	or(B1,B2,ElementShouldBeWater),
+	#=(Element,0,B3),
+	and(B3,ElementShouldBeWater,ElementIsWater),
+
+	#<(RightElement,4,Smaller),
+	#>(RightElement,1,Larger),
+	and(Smaller,Larger,RightIsLeftOrMid),
+	#=(Elem,1,El1),
+	#=(Elem,2,El2),
+	or(El1,El2,ElemMorL),
+	and(RightIsLeftOrMid,ElemMorL,ElementIsLeftOrMid),
+
+	#=(RightElement,0,RightWater),
+	#\=(Elem,1,ElemNotL),
+	and(RightWater,ElemNotL,ElementNotL),
+	sumOfList([ElementIsWater,ElementIsLeftOrMid,ElementNotL],1).
+
+
+
+
+% Ensures that there are only N occurences of a boat of length 2
 check_length2(Solution, N):-
 	List is Solution[1..10,1..10],
 	createOneList(List,OneList),
-	count2([1,3],Solution,H),
+	count2(1,3,OneList,H),
 	transpose(List,Trans),
 	createOneList(Trans,OneList2),
-	count2([4,5],OneList2,V),
-	N #= H+V.
-	
-check_ends(Solution):-
-	count(1,Solution,H),
-	count(3,Solution,H),
-	count(4,Solution,V),
-	count(5,Solution,V).
+	count2(4,5,OneList2,V),
+	sumOfList([H,V],N).
 
-	
+% Ensures that there are only N occurences of a boat of length 3
+check_length3(Solution, N):- 
+	List is Solution[1..10,1..10],
+	createOneList(List,OneList),
+	count3(1,2,3,OneList,H),
+	transpose(List,Trans),
+	createOneList(Trans,OneList2),
+	count3(4,2,5,OneList2,V),
+	sumOfList([H,V],N).
+
+% Ensures that there are only N occurences of a boat of length 4
+check_length4(Solution, N):- 
+	List is Solution[1..10,1..10],
+	createOneList(List,OneList),
+	count4(1,2,2,3,OneList,H),
+	transpose(List,Trans),
+	createOneList(Trans,OneList2),
+	count4(4,2,2,5,OneList2,V),
+	sumOfList([H,V],N).
+
 count_Horizontal2(Solution,H):-
 	count2(1,3,Solution,H).
 	
@@ -72,10 +224,10 @@ count_Vertical2(Solution,N):-
 	count2(4,5,Solution,N).
 
  check_submarines(Solution):-
-    count(6,Solution,4).
+    occurrences(6,Solution,4).
 	
  check_middlepieces(Solution):-
-     count(2,Solution,4).
+     occurrences(2,Solution,4).
 
     
 check_columns([],[]).
@@ -114,7 +266,7 @@ get_element(N,[Elem|Others],Current,Result):-
 	
 check_row(Tally,SolutionRow):-
 	N is (10-Tally),
-	count(0,SolutionRow,N).
+	occurrences(0,SolutionRow,N).
     
 initialize_boats(Hints, Solution):-
     dim(Solution,[10,10]),
@@ -137,12 +289,37 @@ createOneList([H|T],Acc,List):-
     append(Acc,H,R1),
     createOneList(T,R1,List).
 	
-	
-count(X,[],0).
-count(X,[H|T],N):- count(X,T,N1), #=(X,H,B), N#=B+N1.
+count2(_,_,[_],0).
+count2(A,B,[X,Y|T],N):- 
+	count2(A,B,[Y|T],N1), 
+	#=(A,X,B1), 
+	#=(B,Y,B2), 
+	and(B1,B2,B3),
+	N#=B3+N1.
 
-count2(_,_,[],0).
-count2(A,B,[X,Y|T],N):- count2(A,B,T,N1), #=(A,X,B1), #=(B,Y,B2), and(B1,B2,B3),N#=B3+N1.
+
+count3(_,_,_,[_,_],0).
+count3(A,B,C,[X,Y,Z|T],N):- 
+	count3(A,B,C,[Y,Z|T],N1), 
+	#=(A,X,B1), 
+	#=(B,Y,B2), 
+	and(B1,B2,B3),
+	#=(C,Z,B4),
+	and(B3,B4,Btotal),
+	N#=Btotal+N1.
+
+count4(_,_,_,_,[_,_,_],0).
+count4(A,B,C,D,[W,X,Y,Z|T],N):- 
+	count4(A,B,C,D,[X,Y,Z|T],N1), 
+	#=(A,W,B1), 
+	#=(B,X,B2), 
+	and(B1,B2,B3),
+	#=(C,Y,B4),
+	and(B3,B4,B34),
+	#=(D,Z,B5),	
+	and(B34,B5,Btotal),
+	N#=Btotal+N1.
+
 
 sumList([], 0).
 sumList([H|T], Sum) :-
@@ -164,6 +341,17 @@ lists_firsts_rests([[F|Os]|Rest], [F|Fs], [Os|Oss]) :-
         lists_firsts_rests(Rest, Fs, Oss).
 
 
+%% Calculates the sum of all variables in list
+sumOfList(List,Sum):-
+    (
+	foreach(X,List),
+   fromto(Expr,S1,S2,0)
+   do
+   S1 = X + S2
+   ),
+   Sum $= eval(Expr).
+
+
 to_string(0,".").
 to_string(1,"l").
 to_string(2,"m").
@@ -178,238 +366,3 @@ to_string(3,right).
 to_string(4,top).
 to_string(5,bottom).
 to_string(6,circle).
-
-
-
-
-
-
-
-
-:- lib(ic).
-:- lib(lists).
-
-/*
-solve(Puzzle):-
-    T = Puzzle,
-    solve(Puzzle,naive),
-    solve(T,first_fail),
-    solve(Puzzle,middle_out),
-    solve(Puzzle,moff),
-    solve(Puzzle,moffmo).
-*/
-
-solve(P,Search):- 
-                transpose(P,Puzzle),
-                rowConstraint(Puzzle),
-                columnConstraint(Puzzle),
-                blockConstraint(Puzzle),
-                createOneList(Puzzle,R),
-                search(Search,R,B),
-                pretty_print(Puzzle,B,Search).
-
-%Takes a list of lists and returns the concatenation of the lists into one list.                
-createOneList(L,R):-createOneList(L,[],R).
-createOneList([],Acc,Acc).
-createOneList([H|T],Acc,List):-
-    append(Acc,H,R1),
-    createOneList(T,R1,List).
-
-%Each list in the list of lists contains all number from 1 to 9.
-rowConstraint(Puzzle):-
-    (foreach(Row,Puzzle)
-     do
-        Row::1..9,
-        alldifferent(Row) 
-    ).
-    
-/* Pretty Print L */
-pretty_print(List,B,Search) :-
-    (foreach(Row,List)
-    do 
-        write(Row),nl
-	),
-    write(search - Search),nl,    
-    write(backtracks-B),nl,
-    write(-----------------------),nl.
-    
-/* SEARCH */
-search(naive,List,B) :-
-    search(List,0,input_order,indomain,complete,[backtrack(B)]).
-    
-search(middle_out,List,B) :-
-    middle_out(List,MOList),
-    search(MOList,0,input_order,indomain,complete, [backtrack(B)]).
-    
-search(first_fail,List,B) :-
-    search(List,0,first_fail,indomain,complete, [backtrack(B)]).
-    
-search(moff,List,B) :-
-    middle_out(List,MOList),
-    search(MOList,0,first_fail,indomain,complete, [backtrack(B)]).
-    
-search(moffmo,List,B) :-
-    middle_out(List,MOList),
-    search(MOList,0,first_fail,
-    indomain_middle,complete, [backtrack(B)]).    
-    
-        
-% Each column contains all numbers between 1 and 9.    
-columnConstraint(Puzzle):-
-     transpose(Puzzle, Columns),
-     rowConstraint(Columns).
-
-% Transpose matrix - http://stackoverflow.com/questions/4280986/how-to-transpose-a-matrix-in-prolog
-transpose([], []).
-transpose([F|Fs], Ts) :-
-    transpose(F, [F|Fs], Ts).
-
-transpose([], _, []).
-transpose([_|Rs], Ms, [Ts|Tss]) :-
-        lists_firsts_rests(Ms, Ts, Ms1),
-        transpose(Rs, Ms1, Tss).
-
-lists_firsts_rests([], [], []).
-lists_firsts_rests([[F|Os]|Rest], [F|Fs], [Os|Oss]) :-
-        lists_firsts_rests(Rest, Fs, Oss).
-        
-% TODO? scalet niet
-% Constraints for all blocks - http://programmablelife.blogspot.co.at/2012/07/adventures-in-declarative-programming.html
-blockConstraint([]).
-blockConstraint([A,B,C|Rest]):-
-    blocks(A,B,C),
-    blockConstraint(Rest).
-    
-blocks([], [], []).      
-blocks([A,B,C|Bs1], [D,E,F|Bs2], [G,H,I|Bs3]) :-
-  [A,B,C,D,E,F,G,H,I]::1..9,
-  alldifferent([A,B,C,D,E,F,G,H,I]),     
-  blocks(Bs1, Bs2, Bs3).
-  
-  
-%OTHER VIEWPOINT
-solve_2(Puzzle,Search,Numbers):-
-    create_numbers(Numbers),
-    createOneList(Puzzle,List),
-    List::1..9,
-    fill_in_numbers(Puzzle,Numbers),    
-    is_in_each_block(Numbers),
-    search(naive,List,B).        
-        
-% Initializes an array representing each number from 1 to 9 and its positions in the sudoku puzzle.
-create_numbers(Numbers):-
-    dim(Numbers,[9,2]),
-    (for(N,1,9),
-    param(Numbers)
-    do
-        dim(Positions,[9,1]),
-        (for(I,1,9),
-        param(Positions)
-        do
-            A::1..9, 
-            subscript(Positions,[I,1],A)
-        ),
-        Row is Positions[1..9,1],
-        alldifferent(Row),
-    subscript(Numbers,[N,1],N),
-    subscript(Numbers,[N,2],Positions)
-    ).
-
- fill_in_numbers(Puzzle,Numbers):-
-    (for(Row,1,9),
-    param(Numbers),
-    param(Puzzle)
-    do
-        (for(Column,1,9),
-        param(Row),
-        param(Numbers),
-        param(Puzzle)
-        do  
-            get_sudoku_element(Puzzle,Row,Column,Element),
-            (\+(var(Element)) ->
-                            Positions is Numbers[Element,2],
-                            subscript(Positions,[Row,1],Column),
-                            subscript(Numbers,[Element,2],Positions)           
-                            ;
-                            true)
-        )                   
-    ).          
-
-is_in_each_block(Numbers):-
-    (for(N,1,9), %Elk getal afgaan
-    param(Numbers)
-    do
-        Number is Numbers[N,2],
-        (for(BlockRow,1,3), %Elk rij van blokken
-        param(Number),
-        param(Numbers)
-            do
-                Start is 1+((BlockRow-1)*3),
-                Stop is BlockRow*3,
-                RelevantRows is Number[Start..Stop,1],
-                sort_rows(RelevantRows,Sorted),
-                check_correct(Sorted)
-         )
-    ).
-    
-check_correct([A,B,C]):-
-        A#>=1,
-        A#=<3,
-        B#>=4,
-        B#=<6,
-        C#>=7,
-        C#=<9.
-                
-sort_rows([A,B,C],[A,B,C]):-
-    >=(C,B,R1),
-    >=(B,A,R2),
-    R1+R2#>1.
-        
-sort_rows([A,B,C],[A,C,B]):-
-    >=(B,C,R1),
-    >=(C,A,R2),
-    R1+R2#>1.
-    
-sort_rows([A,B,C],[B,C,A]):-
-    >=(A,C,R1),
-    >=(C,B,R2),
-    R1+R2#>1.
-    
-sort_rows([A,B,C],[B,A,C]):-
-    >=(C,A,R1),
-    >=(A,B,R2),
-    R1+R2#>1.
-    
-sort_rows([A,B,C],[C,A,B]):-
-    >=(B,A,R1),
-    >=(A,C,R2),
-    R1+R2#>1.
-    
-sort_rows([A,B,C],[C,B,A]):-
-    >=(A,B,R1),
-    >=(B,C,R2),
-    R1+R2#>1.
-                            
- %Retrieves the element at given Row and given Column.
- get_sudoku_element(Puzzle,Row,Column,Element):-get_sudoku_element(Puzzle,1,Row,Column,Element).
- 
- get_sudoku_element([_|RestOfPuzzle],CurrentRow,Row,Column,Element):-
-    CurrentRow<Row,
-    NewRow is CurrentRow+1,
-    get_sudoku_element(RestOfPuzzle,NewRow,Row,Column,Element).
-    
- get_sudoku_element([PuzzleRow|_],CurrentRow,Row,Column,Element):-
-    CurrentRow=:=Row,
-    get_element(PuzzleRow,Column,Element).
-    
-get_element(List,N,Elem):-
-    get_element(N,List,0,Elem).
-    
-get_element(_,[],_,_).
-get_element(N,[Elem|Others],Current,Result):-
-    Next is Current+1,
-    (Next==N ->
-        Result = Elem
-    ;
-        get_element(N,Others,Next,Result)
-    ).
